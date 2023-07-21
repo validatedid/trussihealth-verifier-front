d40ae08ea4f076fd4d2c8de6632433df414fd59f
Mauro Lucchini <maurolucchinidepompa@gmail.com>
2023-07-21 10:38:10 +0200

    ci: root directory

--- trussihealth-verifier-back/src/modules/shared/infrastructure/vidchainApi/healthData.ts
+++
@@ -1,9 +0,0 @@
-import { AuthenticationVIDchainAPI } from './authenticationVIDchainAPI';
-
-
-export class HealthData extends AuthenticationVIDchainAPI {
-  async getData(documentId: string): Promise<unknown> {
-    const authorization = await this.getAuthorizationBasic();
-    return await this.vidchainAPIConnector.getData(documentId, authorization);
-  }
-}
